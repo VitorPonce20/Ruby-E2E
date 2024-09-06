4efa7812bb90e444a3d128024c482c82f755b550
Vitor Ponce <vitor.ponce20@gmail.com>
2024-09-05 23:51:36 -0300

    added step definition for remove itens steps

---
+++ features/step_definitions/addItemsStep.rb
@@ -0,0 +1,11 @@
+Given('login') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('select the first item') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('this one is added to the cart') do    
+    pending # Write code here that turns the phrase above into concrete actions
+  end
---
+++ features/step_definitions/removeItens.rb
@@ -0,0 +1,15 @@
+Given('access itens main page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I select all itens for remove') do    
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('remove all itens') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('go back home page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
