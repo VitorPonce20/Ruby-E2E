9679e30f8d90751752038fcccbcadaf62de84f20
Vitor Ponce <vitor.ponce20@gmail.com>
2024-09-09 23:30:08 -0300

    separete remove itens scenario

--- features/step_definitions/addItemsStep.rb
+++ features/step_definitions/addItemsStep.rb
@@ -1,5 +1,16 @@
 Given('login') do
-    pending # Write code here that turns the phrase above into concrete actions
+  @driver = Selenium::WebDriver.for :chrome
+  @driver.get "https://www.saucedemo.com/"
+  
+  @driver.find_element(:id, "user-name").send_keys("standard_user")
+  @driver.find_element(:id, "password").send_keys("secret_sauce")
+  @driver.find_element(:id, "login-button").click    
+  
+  @driver.find_element(:id, "add-to-cart-sauce-labs-backpack").click
+  @driver.find_element(:id, "add-to-cart-sauce-labs-bike-light").click
+  @driver.find_element(:id, "add-to-cart-sauce-labs-bolt-t-shirt").click
+  @driver.find_element(:id, "add-to-cart-sauce-labs-fleece-jacket").click
+  @driver.find_element(:id, "add-to-cart-test.allthethings()-t-shirt-(red)").click
   end
   
   When('select the first item') do
--- features/step_definitions/removeItens.rb
+++ features/step_definitions/removeItens.rb
@@ -1,15 +1,20 @@
 Given('access itens main page') do
-    pending # Write code here that turns the phrase above into concrete actions
+  @driver.find_element(:id, "shopping_cart_container").click    
+  @driver.find_element(:id, "remove-sauce-labs-backpack").click
+  
   end
   
   When('I select all itens for remove') do    
-    pending # Write code here that turns the phrase above into concrete actions
+    @driver.find_element(:id, "remove-sauce-labs-bike-light").click
+    @driver.find_element(:id, "remove-sauce-labs-bolt-t-shirt").click
+   
   end
   
   Then('remove all itens') do
-    pending # Write code here that turns the phrase above into concrete actions
+    @driver.find_element(:id, "remove-sauce-labs-fleece-jacket").click
+   
   end
   
   Then('go back home page') do
-    pending # Write code here that turns the phrase above into concrete actions
+    @driver.find_element(:id, "remove-test.allthethings()-t-shirt-(red)").click
   end
